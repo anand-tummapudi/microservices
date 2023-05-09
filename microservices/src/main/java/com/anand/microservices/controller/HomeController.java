c01fca42c3f02ab94a6fa695b39e4b6e47a7f676
anand tummapudi <anand.tummapudi@gmail.com>
2023-05-09 05:45:38 +0530

    updated read me

--- microservices/src/main/java/com/anand/microservices/controller/HomeController.java
+++ microservices/src/main/java/com/anand/microservices/controller/HomeController.java
@@ -1,8 +1,8 @@
 package com.anand.microservices.controller;
 
+import com.anand.microservices.model.User;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @ResponseBody
@@ -10,7 +10,21 @@ public class HomeController {
 
     @RequestMapping("/")
     public String home(){
+
         return "Hello World!";
+    }
+
+    //@RequestMapping(value="/user", method = RequestMethod.GET)
+    @GetMapping("/user")
+    public User user(){
+        User user1 = new User("123","Anand","anjiammu@gmail.com");
+
+        return user1;
+    }
+
+    @GetMapping("/{id}/{id2}")
+    public String pathVariable(@PathVariable String id, @PathVariable("id2") String name){
+        return "The path variable is:"+id+" and :"+name;
     }
 
 }
