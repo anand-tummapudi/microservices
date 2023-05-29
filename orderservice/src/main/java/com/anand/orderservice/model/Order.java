9b2b6c8ef66ab2527fc7aa9f66d49e50d777d037
Anand <anand.tummapudi@gmail.com>
2023-05-29 00:38:51 -0700

    order service updated

---
+++ orderservice/src/main/java/com/anand/orderservice/model/Order.java
@@ -0,0 +1,38 @@
+package com.anand.orderservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.Instant;
+
+
+@Entity
+@Table(name="ORDER_DETAILS")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private long id;
+    @Column(name="ORDER_ID")
+    private long orderId;
+
+    @Column(name="QUANTITY")
+    private int quantity;
+
+    @Column(name="ORDER_DATE")
+    private Instant orderDate;
+
+    @Column(name="STATUS")
+    private String orderStatus;
+
+    @Column(name="TOTAL_AMOUNT")
+    private double amount;
+
+}
---
+++ orderservice/src/main/java/com/anand/orderservice/service/OrderService.java
@@ -0,0 +1,4 @@
+package com.anand.orderservice.service;
+
+public interface OrderService {
+}
