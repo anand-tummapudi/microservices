a3fb4794f8a8c6b41e81b8d56db18fa92ccac336
Anand <anand.tummapudi@gmail.com>
2023-06-11 08:01:35 -0700

    updated security

--- spring-security/src/main/java/com/example/springsecurity/SecurityConfiguration.java
+++ spring-security/src/main/java/com/example/springsecurity/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.example.springsecurity;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cglib.proxy.NoOp;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -7,22 +8,47 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import javax.sql.DataSource;
+
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    DataSource dataSource;
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-       auth.inMemoryAuthentication()
-               .withUser("user")
-               .password("password")
-               .roles("USER")
-               .and()
-               .withUser("user2")
-               .password("password")
-               .roles("ADMIN");
+        // In Memory Authentication Example
+//        auth.inMemoryAuthentication()
+//               .withUser("user")
+//               .password("password")
+//               .roles("USER")
+//               .and()
+//               .withUser("user2")
+//               .password("password")
+//               .roles("ADMIN");
 
+        // Default Schema
+        /*auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .withDefaultSchema()
+                .withUser(
+                        User.withUsername("user")
+                                .password("password")
+                                .roles("USER")
+                )
+                .withUser(
+                        User.withUsername("adminuser")
+                                .password("password")
+                                .roles("ADMIN")
+                );
+        */
+        // JDBC schema with user provided schema and data
+        auth.jdbcAuthentication()
+                .dataSource(dataSource);
     }
 
     @Override
